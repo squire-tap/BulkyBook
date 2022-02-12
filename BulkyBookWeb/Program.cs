b7d421e7aa7ebd4bdabb264f461ced4d736ebdb6
Luis Henrique <luislopesteo@hotmail.com>
2022-02-12 17:21:39 -0300

    Criacao das interfaces

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,4 +1,6 @@
 using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
